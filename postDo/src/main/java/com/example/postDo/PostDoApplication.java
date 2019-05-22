7d0f03573171d8108f153c42b41ed3ff67c4f6f1
Mitra Pantic <mitra196506@gmail.com>
2019-05-22 16:26:50 +0200

    Izmena u objektima

--- postDo/src/main/java/com/example/postDo/PostDoApplication.java
+++ postDo/src/main/java/com/example/postDo/PostDoApplication.java
@@ -2,8 +2,10 @@ package com.example.postDo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.postDo.controller"})
 public class PostDoApplication {
 
 	public static void main(String[] args) {
