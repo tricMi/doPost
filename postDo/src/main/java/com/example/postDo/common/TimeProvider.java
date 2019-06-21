924ab35cd2f73418dbc26a5615a9e2c5a55ba8ee
Aleksandar Budimirovic <aleksandar.budimirovic97@gmail.com>
2019-06-21 15:38:09 +0200

    doradjen security

---
+++ postDo/src/main/java/com/example/postDo/common/TimeProvider.java
@@ -0,0 +1,9 @@
+package com.example.postDo.common;
+
+import java.util.Date;
+
+public class TimeProvider {
+	public Date now() {
+		return new Date();
+	}
+}
---
+++ postDo/src/main/java/com/example/postDo/security/TokenHelper.java
@@ -0,0 +1,135 @@
+package com.example.postDo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+ 
+import com.example.postDo.common.TimeProvider;
+ 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Component
+public class TokenHelper {
+ 
+    @Value("postDoServer")
+    private String APP_NAME;
+ 
+    @Value("postDo")
+    public String SECRET;
+ 
+    @Value("3000")
+    private int EXPIRES_IN;
+ 
+    @Value("Authorization")
+    private String AUTH_HEADER;
+ 
+    static final String AUDIENCE = "chocuser";
+ 
+    @Autowired
+    TimeProvider timeProvider;
+ 
+    // HMAC sa SHA-512 hash algoritmom
+    private SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS512;
+ 
+    // Metoda koja iz sadrzaja tokena (claims) pronalazi username korisnika
+    public String getUsernameFromToken(String token) {
+        String username;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            username = claims.getSubject();
+        } catch (Exception e) {
+            username = null;
+        }
+        return username;
+    }
+ 
+    public Date getIssuedAtDateFromToken(String token) {
+        Date issueAt;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            issueAt = claims.getIssuedAt();
+        } catch (Exception e) {
+            issueAt = null;
+        }
+        return issueAt;
+    }
+ 
+    public String getAudienceFromToken(String token) {
+        String audience;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            audience = claims.getAudience();
+        } catch (Exception e) {
+            audience = null;
+        }
+        return audience;
+    }
+ 
+    public String refreshToken(String token) {
+        String refreshedToken;
+        Date a = timeProvider.now();
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            claims.setIssuedAt(a);
+            refreshedToken = Jwts.builder().setClaims(claims).setExpiration(generateExpirationDate())
+                    .signWith(SIGNATURE_ALGORITHM, SECRET).compact();
+        } catch (Exception e) {
+            refreshedToken = null;
+        }
+        return refreshedToken;
+    }
+ 
+    // Funkcija koja pri autentifikaciji korisnika kreira novi JWT token
+    public String generateToken(String username) {
+        return Jwts.builder().setIssuer(APP_NAME).setSubject(username).setAudience(AUDIENCE)
+                .setIssuedAt(timeProvider.now()).setExpiration(generateExpirationDate())
+                .signWith(SIGNATURE_ALGORITHM, SECRET).compact();
+    }
+ 
+    private Claims getAllClaimsFromToken(String token) {
+        Claims claims;
+        try {
+            claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            claims = null;
+        }
+        return claims;
+    }
+   
+    public long getExiprationDate(String token) {
+        return getAllClaimsFromToken(token).getExpiration().getTime();
+    }
+ 
+    private Date generateExpirationDate() {
+        return new Date(timeProvider.now().getTime() + EXPIRES_IN * 1000);
+    }
+   
+    public int getExpiredIn() {
+        return EXPIRES_IN;
+    }
+ 
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username != null && username.equals(userDetails.getUsername()));
+    }
+ 
+    public String getToken(HttpServletRequest request) {
+        /**
+         * Getting the token from Authentication header e.g Bearer your_token
+         */
+        String authHeader = getAuthHeaderFromHeader(request);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+ 
+        return null;
+    }
+ 
+    public String getAuthHeaderFromHeader(HttpServletRequest request) {
+        return request.getHeader(AUTH_HEADER);
+    }
